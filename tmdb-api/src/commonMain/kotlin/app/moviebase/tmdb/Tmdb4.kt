454421ec81afb7221754a14675f2791ebd5462ae
Christian Krüger <15275971+chrisnkrueger@users.noreply.github.com>
2021-05-21 12:00:10 +0200

    Add LogLevel to Tmdb4

--- tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/Tmdb4.kt
+++ tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/Tmdb4.kt
@@ -3,25 +3,27 @@ package app.moviebase.tmdb
 import app.moviebase.tmdb.api.Tmdb4AccountApi
 import app.moviebase.tmdb.api.Tmdb4AuthenticationApi
 import app.moviebase.tmdb.api.Tmdb4ListApi
+import app.moviebase.tmdb.remote.TmdbLogLevel
 import app.moviebase.tmdb.remote.buildHttpClient
 import io.ktor.client.request.*
 
 class Tmdb4(
     tmdbApiKey: String,
-    authenticationToken: String? = null
+    authenticationToken: String? = null,
+    logLevel: TmdbLogLevel = TmdbLogLevel.NONE
 ) {
 
     var accessToken: String? = null
     var requestToken: String? = null
 
-    private val client = buildHttpClient {
+    private val client = buildHttpClient(logLevel) {
         it.parameter(TmdbUrlParameter.API_KEY, tmdbApiKey)
         accessToken?.let { token ->
             it.header("Authorization", "Bearer $token")
         }
     }
 
-    private val authClient = buildHttpClient {
+    private val authClient = buildHttpClient(logLevel) {
         it.parameter(TmdbUrlParameter.API_KEY, tmdbApiKey)
         requireNotNull(authenticationToken) { "authentication token not set for request auth endpoints" }
         it.header("Authorization", "Bearer $authenticationToken")
