92cf0ca634853d3d67adb93931ce78f1b8d79fef
Christian Krüger <15275971+chrisnkrueger@users.noreply.github.com>
2021-09-25 17:19:44 +0200

    Bump versions

--- README.md
+++ README.md
@@ -11,8 +11,8 @@
 [![Maven Central](https://img.shields.io/maven-central/v/app.moviebase/tmdb-api?label=Maven%20Central)](https://search.maven.org/artifact/app.moviebase/tmdb-api)
 [![Build Status](https://app.bitrise.io/app/0e5dcdc490429c81/status.svg?token=dRSlP2lXiMSQ8keWh-o8mQ&branch=master)](https://app.bitrise.io/app/0e5dcdc490429c81)
 [![Issues](https://img.shields.io/github/issues/MoviebaseApp/tmdb-api/total)](http://kotlinlang.org)
-[![Kotlin](https://img.shields.io/badge/kotlin-1.4.32-blue.svg?logo=kotlin)](http://kotlinlang.org)
-[![Gradle](https://img.shields.io/badge/Gradle-6-green?style=flat)](https://gradle.org)
+[![Kotlin](https://img.shields.io/badge/kotlin-1.5.30-blue.svg?logo=kotlin)](http://kotlinlang.org)
+[![Gradle](https://img.shields.io/badge/Gradle-7-green?style=flat)](https://gradle.org)
 [![GitHub License](https://img.shields.io/badge/license-Apache%20License%202.0-blue.svg?style=flat)](http://www.apache.org/licenses/LICENSE-2.0)
 
 <hr>
@@ -43,7 +43,7 @@ To use the library in a single-platform project, add a dependency.
 
 ```kotlin
 dependencies {
-    implementation("app.moviebase:tmdb-api:0.5.0")
+    implementation("app.moviebase:tmdb-api:0.6.0")
 }
 ```
 
@@ -52,7 +52,7 @@ In Kotlin Multiplatform projects, add the dependency to your commonMain source-s
 ```kotlin
 commonMain {
     dependencies {
-        implementation("app.moviebase:tmdb-api:0.5.0")
+        implementation("app.moviebase:tmdb-api:0.6.0")
     }
 }
 ``` 
@@ -65,7 +65,7 @@ Add a dependency to the `<dependencies>` element.
 <dependency>
     <groupId>app.moviebase</groupId>
     <artifactId>tmdb-api</artifactId>
-    <version>0.5.0</version>
+    <version>0.6.0</version>
 </dependency>
 ```
 
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,8 @@
 object Versions {
 
     val versionMajor = 0
-    val versionMinor = 5
-    val versionPatch = 4
+    val versionMinor = 6
+    val versionPatch = 0
 
     val versionName = "$versionMajor.$versionMinor.$versionPatch"
 
@@ -12,17 +12,21 @@ object Versions {
     val swiftpackage = "2.0.3"
 
     // Kotlin
-    val kotlin = "1.4.32"
-    val coroutines = "1.4.3"
-    val kotlinxDatetime = "0.1.1"
-    val serialization = "1.1.0"
+    val kotlin = "1.5.30"
+    val coroutines = "1.5.2"
+    val kotlinxDatetime = "0.2.1"
+    val serialization = "1.3.0"
     val kotlinIo = "0.1.16"
 
-    // Testing
-    val junitJupiter = "5.7.0"
-
     // Data
-    val ktor = "1.5.3"
+    val ktor = "1.6.3"
+
+    // Testing
+    val junitJupiter = "5.8.0-RC1"
+    val truth = "1.1.3"
+    val junit = "4.13.2"
+    val mockito = "3.12.4"
+    val mockitoKotlin = "3.2.0"
 }
 
 object Plugins {
@@ -52,12 +56,11 @@ object Libs {
     object Testing {
         val coroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}"
         val ktorClientMock = "io.ktor:ktor-client-mock:${Versions.ktor}"
-        val truth = "com.google.truth:truth:1.1.2"
-        val junit = "junit:junit:4.13.2"
+        val truth = "com.google.truth:truth:${Versions.truth}"
+        val junit = "junit:junit:${Versions.junit}"
         val jupiter = "org.junit.jupiter:junit-jupiter-api:${Versions.junitJupiter}"
         val jupiterEngine = "org.junit.jupiter:junit-jupiter-engine:${Versions.junitJupiter}"
-        val mockito = "org.mockito:mockito-inline:3.8.0"
-        val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:2.2.0"
+        val mockito = "org.mockito:mockito-inline:${Versions.mockito}"
+        val mockitoKotlin = "org.mockito.kotlin:mockito-kotlin:${Versions.mockitoKotlin}"
     }
-
 }
