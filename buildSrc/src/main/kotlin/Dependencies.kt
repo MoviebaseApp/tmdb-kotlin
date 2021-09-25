116091fccf77a6aea8f50265bdc39a40082f79bd
Christian Krüger <15275971+chrisnkrueger@users.noreply.github.com>
2021-09-25 11:27:32 +0200

    Update gradle version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,7 +2,7 @@ object Versions {
 
     val versionMajor = 0
     val versionMinor = 5
-    val versionPatch = 3
+    val versionPatch = 4
 
     val versionName = "$versionMajor.$versionMinor.$versionPatch"
 
