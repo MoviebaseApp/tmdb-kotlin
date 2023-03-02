ea2fa61643cefb5c242b003391dfdc3efddac126
Chris Banes <chris@banes.me>
2023-03-02 12:50:02 +0000

    Add support for /configuration endpoint

--- tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/api/TmdbConfigurationApi.kt
+++ tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/api/TmdbConfigurationApi.kt
@@ -1,8 +1,15 @@
 package app.moviebase.tmdb.api
 
+import app.moviebase.tmdb.model.TmdbConfiguration
+import app.moviebase.tmdb.remote.endPointV3
 import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
 
 class TmdbConfigurationApi(private val client: HttpClient) {
 
+    suspend fun getApiConfiguration(): TmdbConfiguration = client.get {
+        endPointV3("configuration")
+    }.body()
 
 }
