6e2a644c2a94762d344e8cbdcec92a7208d31911
Chris Banes <chris@banes.me>
2023-03-02 12:50:02 +0000

    Add support for /configuration endpoint

--- tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/model/TmdbConfigurationModel.kt
+++ tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/model/TmdbConfigurationModel.kt
@@ -1,2 +1,21 @@
 package app.moviebase.tmdb.model
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TmdbConfiguration(
+    @SerialName("images") val images: TmdbImagesConfiguration,
+    @SerialName("change_keys") val changeKeys: List<String>,
+)
+
+@Serializable
+data class TmdbImagesConfiguration(
+    @SerialName("base_url") val baseUrl: String,
+    @SerialName("secure_base_url") val secureBaseUrl: String,
+    @SerialName("backdrop_sizes") val backdropSizes: List<String> = emptyList(),
+    @SerialName("poster_sizes") val posterSizes: List<String> = emptyList(),
+    @SerialName("logo_sizes") val logoSizes: List<String> = emptyList(),
+    @SerialName("profile_sizes") val profileSizes: List<String> = emptyList(),
+    @SerialName("still_sizes") val stillSizes: List<String> = emptyList(),
+)
---
+++ tmdb-api/src/jvmTest/kotlin/app/moviebase/tmdb/api/TmdbConfigurationApiTest.kt
@@ -0,0 +1,33 @@
+package app.moviebase.tmdb.api
+
+import app.moviebase.tmdb.remote.mockHttpClient
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+
+class TmdbConfigurationApiTest {
+
+    val client = mockHttpClient(
+        version = 3,
+        responses = mapOf(
+            "configuration" to "configuration/configuration.json",
+        )
+    )
+
+    val api = TmdbConfigurationApi(client)
+
+    @Test
+    fun `it can fetch api configuration`() = runBlocking {
+        val config = api.getApiConfiguration()
+
+        assertThat(config.changeKeys).isNotEmpty()
+
+        assertThat(config.images.baseUrl).isNotEmpty()
+        assertThat(config.images.secureBaseUrl).isNotEmpty()
+        assertThat(config.images.backdropSizes).isNotEmpty()
+        assertThat(config.images.posterSizes).isNotEmpty()
+        assertThat(config.images.logoSizes).isNotEmpty()
+        assertThat(config.images.profileSizes).isNotEmpty()
+        assertThat(config.images.stillSizes).isNotEmpty()
+    }
+}
