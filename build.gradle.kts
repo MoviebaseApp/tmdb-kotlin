d694b8759bd240fb66ba4c75a4716b9c9d370767
Christian Krueger <15275971+chrisnkrueger@users.noreply.github.com>
2023-03-09 00:38:31 +0100

    Add new maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,6 @@ import com.diffplug.gradle.spotless.SpotlessExtension
 plugins {
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.kotlin.multiplatform) apply false
-    alias(libs.plugins.nexus) apply true
     alias(libs.plugins.dokka) apply false
     alias(libs.plugins.ben.manes.versions) apply false
     alias(libs.plugins.spotless) apply false
@@ -14,18 +13,6 @@ buildscript {
         gradlePluginPortal()
         google()
         mavenCentral()
-    }
-}
-
-nexusPublishing {
-    repositories {
-        sonatype {
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
-            username.set(findProperty("SONATYPE_USER") as String?)
-            password.set(findProperty("SONATYPE_PASSWORD") as String?)
-            stagingProfileId.set(findProperty("SONATYPE_STAGING_PROFILE_ID_MOVIEBASE") as String?)
-        }
     }
 }
 
