56a11fab4fcf2556909966c53ed483dab7ddcfd9
Dmitriy <dekanszn@gmail.com>
2023-04-10 17:08:58 +0400

    Fix `trending` api, add simple tests for it

--- tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/api/TmdbTrendingApi.kt
+++ tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/api/TmdbTrendingApi.kt
@@ -1,10 +1,10 @@
 package app.moviebase.tmdb.api
 
-import app.moviebase.tmdb.model.*
-import app.moviebase.tmdb.core.endPointV4
+import app.moviebase.tmdb.core.endPointV3
 import app.moviebase.tmdb.core.parameterLanguage
 import app.moviebase.tmdb.core.parameterPage
 import app.moviebase.tmdb.core.parameterRegion
+import app.moviebase.tmdb.model.*
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
@@ -17,7 +17,7 @@ class TmdbTrendingApi internal constructor(private val client: HttpClient) {
         language: String? = null,
         region: String? = null
     ): TmdbMoviePageResult = client.get {
-        endPointV4("trending", TmdbRequestMediaType.MOVIE.value, timeWindow.value)
+        endPointV3("trending", TmdbRequestMediaType.MOVIE.value, timeWindow.value)
         parameterLanguage(language)
         parameterRegion(region)
         parameterPage(page)
@@ -29,7 +29,19 @@ class TmdbTrendingApi internal constructor(private val client: HttpClient) {
         language: String? = null,
         region: String? = null
     ): TmdbShowPageResult = client.get {
-        endPointV4("trending", TmdbRequestMediaType.TV.value, timeWindow.value)
+        endPointV3("trending", TmdbRequestMediaType.TV.value, timeWindow.value)
+        parameterLanguage(language)
+        parameterRegion(region)
+        parameterPage(page)
+    }.body()
+
+    suspend fun getTrendingPeople(
+        timeWindow: TmdbTimeWindow,
+        page: Int,
+        language: String? = null,
+        region: String? = null
+    ): TmdbPersonPageResult = client.get {
+        endPointV3("trending", TmdbRequestMediaType.PERSON.value, timeWindow.value)
         parameterLanguage(language)
         parameterRegion(region)
         parameterPage(page)
