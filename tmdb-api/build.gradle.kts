5fc998d22a59dc18ac83a8c9a8b8a3d918bc5555
Chris Banes <chris@banes.me>
2023-05-16 22:03:57 +0100

    Add iOS targets
    Just needed to add the relevant targets and source sets.
    Switched CI to run on MacOS as iOS builds need to be built on MacOS.
    
    Had to downgrade Coroutines due to
    https://youtrack.jetbrains.com/issue/KTOR-5728/

--- tmdb-api/build.gradle.kts
+++ tmdb-api/build.gradle.kts
@@ -15,6 +15,15 @@ kotlin {
         browser()
         nodejs()
     }
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64(),
+    ).forEach {
+        it.binaries.framework {
+            baseName = "app-moviebase-tmdb-api"
+        }
+    }
 
     sourceSets {
         val commonMain by getting {
@@ -60,6 +69,34 @@ kotlin {
         }
 
         val jsMain by getting {
+        }
+
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+
+        val iosMain by creating {
+            dependsOn(commonMain)
+
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+
+            dependencies {
+                implementation(libs.ktor.darwin)
+            }
+        }
+
+        val iosX64Test by getting
+        val iosArm64Test by getting
+        val iosSimulatorArm64Test by getting
+
+        val iosTest by creating {
+            dependsOn(commonTest)
+
+            iosX64Test.dependsOn(this)
+            iosArm64Test.dependsOn(this)
+            iosSimulatorArm64Test.dependsOn(this)
         }
     }
 }
