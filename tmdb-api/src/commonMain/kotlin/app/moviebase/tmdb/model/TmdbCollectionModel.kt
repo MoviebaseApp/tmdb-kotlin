411437084ea6b035df8d1715421a5114fac15a9d
Christian Krueger <15275971+chrisnkrueger@users.noreply.github.com>
2024-04-05 08:10:00 +0200

    Fix optional name and original_name in TmdbCollection

--- tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/model/TmdbCollectionModel.kt
+++ tmdb-api/src/commonMain/kotlin/app/moviebase/tmdb/model/TmdbCollectionModel.kt
@@ -8,10 +8,10 @@ import kotlinx.serialization.Serializable
 data class TmdbCollection(
     @SerialName("adult") val adult: Boolean,
     @SerialName("id") override val id: Int,
-    @SerialName("name") val name: String,
+    @SerialName("name") val name: String? = null,
     @SerialName("backdrop_path") val backdropPath: String? = null,
     @SerialName("original_language") val originalLanguage: String,
-    @SerialName("original_name") val originalName: String,
+    @SerialName("original_name") val originalName: String? = null,
     @SerialName("overview") val overview: String,
     @SerialName("poster_path") val posterPath: String?
 ) : TmdbSearchable, TmdbSearchableListItem
